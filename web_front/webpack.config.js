b9878f2d3ffbece877424db2b6635f455b8482c7
fo-pluto <2818151216@qq.com>
2023-03-16 20:55:32 +0800

    yeah, the bug has been solved today, hahaha!!

--- web_front/webpack.config.js
+++ web_front/webpack.config.js
@@ -1,8 +1,7 @@
 module.exports = {
     // ...其他配置
     resolve: {
-        fallback: {
-        "path": require.resolve("path-browserify")
-        }
+        fallback: { 'path': require.resolve('path-browserify') },
+        extensions: ['.jsx', '.js', '.tsx', '.ts'],
     }
 };
